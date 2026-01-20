d71e98a612bec50c428bcf72bc5b6502b0ab16da
kwmbe <koberkein@gmail.com>
2026-01-20 22:18:05 +0100

    Test hosting portfolio

--- site.go
+++ site.go
@@ -4,19 +4,22 @@ import (
   "log"
   "net/http"
 
-  // "github.com/gin-gonic/contrib/static"
+  "github.com/gin-gonic/contrib/static"
   "github.com/gin-gonic/gin"
 )
 
 func main() {
-  // gin.SetMode(gin.ReleaseMode)
+  gin.SetMode(gin.ReleaseMode)
 
   r := gin.Default()
 
   // Define a health GET endpoint
-  r.GET("/health", func(c *gin.Context) {
+  r.Any("/health", func(c *gin.Context) {
   	c.Status(http.StatusNoContent)
   })
+
+  // Static file hosting for portfolio
+  r.Use(static.Serve("/", static.LocalFile("./portfolio/build/", true)))
 
   // Start server on port 80 (may need elevated permissions)
   // Server will listen on localhost

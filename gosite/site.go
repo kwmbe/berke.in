06e1f4f9a82a6a40ae043267d7edd58a8d36f74f
kwmbe <koberkein@gmail.com>
2026-01-25 11:41:43 +0100

    Don't allow directory traversal

--- gosite/site.go
+++ gosite/site.go
@@ -19,7 +19,10 @@ func main() {
   })
 
   // Static file hosting for portfolio
-  r.Use(static.Serve("/", static.LocalFile("./portfolio", true)))
+  r.Use(static.Serve("/", static.LocalFile("./portfolio", false)))
+	r.GET("/", func(c *gin.Context) {
+		c.File("./portfolio/index.html")
+	})
 
   // Start server on port 80 (may need elevated permissions)
   // Server will listen on localhost
